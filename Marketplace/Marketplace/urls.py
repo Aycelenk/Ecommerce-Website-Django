c588ac88b6e3da89ca37354c3ca6db6ea794cfea
caltinuzengi <91156123+caltinuzengi@users.noreply.github.com>
2023-03-31 04:04:18 +0300

    database-models-marketplace products cardviews

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from Main.views import index,login,signup
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/",signup,name = "signup"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
