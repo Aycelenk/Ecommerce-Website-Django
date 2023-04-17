3038c3bd60de512424d52397024e8ccfbfd4514a
dodo146 <digdeci@gmail.com>
2023-04-17 10:08:03 +0300

    Added crispy forms to make forms smooth

--- Marketplace/Marketplace/settings.py
+++ Marketplace/Marketplace/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "Main",
     "Product",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -127,6 +129,8 @@ LOGIN_URL = "/login/"
 LOGOUT_REDIRECT_URL = "/"
 
 AUTH_USER_MODEL = 'Product.Users'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
