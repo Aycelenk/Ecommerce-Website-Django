ce5be7d22eca2053e8437b7ad11ca12c0f478a65
mustafa cem <mustafacem@sabanciuniv.edu>
2023-05-05 18:12:55 +0300

    kenya

--- Marketplace/Cart/views.py
+++ Marketplace/Cart/views.py
@@ -6,6 +6,99 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 
+from fpdf import FPDF
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def create_pdf(name,items,email):
+    class PDF(FPDF):
+        def header(self):
+            # Logo
+            self.image('download.png', 10, 8, 25)
+            # font
+            self.set_font('helvetica', 'B', 20)
+            # Padding
+            self.cell(40)
+            # Title
+            self.cell(120, 10, 'Thanks for Shoping with us', border=True, ln=1, align='C')
+            # Line break
+            self.ln(20)
+
+        # Page footer
+        def footer(self):
+            # Set position of the footer
+            self.set_y(-15)
+            # set font
+            self.set_font('helvetica', 'I', 8)
+            # Set font color grey
+            self.set_text_color(169, 169, 169)
+            # Page number
+            self.cell(0, 10, f'Page {self.page_no()}', align='C')
+
+    # Create a PDF object
+    pdf = PDF('P', 'mm', 'Letter')
+
+    # get total page numbers
+    pdf.alias_nb_pages()
+
+    # Set auto page break
+    pdf.set_auto_page_break(auto = True, margin = 15)
+
+    #Add Page
+    pdf.add_page()
+
+    # specify font
+    pdf.set_font('helvetica', 'BIU', 16)
+
+    pdf.set_font('times', '', 12)
+    pdf.cell(0, 10, f'Dear '+ name +' thanks for chossing us', ln=1)
+    pdf.cell(0, 10, f'', ln=1)
+
+    pdf.cell(0, 10, f'Dear ' + name + ' Here your items: ', ln=1)
+    for i in range(len(items)//3):
+        pdf.cell(0, 10, f'item: '+ items[i*3]+'     amount: '+ items[i*3+1] +'    price: '+items[i*3+2]+'', ln=1)
+
+    pdf.output('pdf_2.pdf')
+
+    print("*****************************************************************************")
+    mail_content = '''Bizzi tercih etdiğiniz için teşşekür ederiz
+    '''
+    sender_address = 'cs308shopping@gmail.com'
+    sender_pass = 'oeiuyzfbjqvzvimc'
+    receiver_address = email
+
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'test'
+    # The subject line
+    # The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    attach_file_name = 'pdf_2.pdf'
+    attach_file = open(attach_file_name, 'rb')  # Open the file as binary mode
+    payload = MIMEBase('application', 'octate-stream')
+    payload.set_payload((attach_file).read())
+    encoders.encode_base64(payload)  # encode the attachment
+    # add payload header with filename
+    payload.add_header('Content-Decomposition', 'attachment', filename=attach_file_name)
+    payload.add_header('Content-Disposition', "attachment; filename= %s" % attach_file_name)
+    message.attach(payload)
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+
+    session.login(sender_address, sender_pass)  # login with mail_id and password
+
+
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+
+
 def cart(request):
     if request.user.is_authenticated:
         check_anonymous_cart_products(request)
@@ -64,6 +157,12 @@ def buy(request):
         record_count = OrderedProduct.objects.count()
         product = get_object_or_404(InStockProduct,pk = product_id)
         user = request.user
+
+
+        lst = [str(product), str(record_count), str(product.price)]
+        #create_pdf(" to me ", lst, "cs308shopping@gmail.com")
+        #create_pdf(str(user), lst, str(user.email))
+
         ordered_item = OrderedProduct.objects.create(ID = record_count + 1,name= product.name,model=product.model,
         number=product.number,description=product.description,price=product.price,
         warranty_status=product.warranty_status,distributor_info=product.distributor_info,
@@ -73,5 +172,5 @@ def buy(request):
         messages.success(request,f"Product is bought successfully.You can check the delivery process in delivery tab")
         product.quantity_in_stocks -= quantity
         product.save()
-        return render(request,"buy.html",{"product":ordered_item})
+        return render(request,"buy.html",{"product":product})
 
