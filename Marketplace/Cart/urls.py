ab82cb858bb274b96b4f05874541b9659ddeec79
aycelen <aycelenkaptan@sabanciuniv.edu>
2023-06-12 02:36:30 +0300

    chart made

--- Marketplace/Cart/urls.py
+++ Marketplace/Cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.cart,name="cart"),
     path("buy/",views.buy,name="buy"),
-    path("card_info",views.card_info, name="card_info")
-
+    path("card_info",views.card_info, name="card_info"),
+    path("chart",views.chart, name="chart")
 ]
